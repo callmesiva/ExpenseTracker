497a2ea0e50dd218c87479a83b78de1e45028bc5
callmesiva <sgnathan10@gmail.com>
2022-12-05 17:24:00 +0530

    store signup data to db

--- server/controller/expenceControl.js
+++ server/controller/expenceControl.js
@@ -1,3 +1,6 @@
+const db = require("../database")
+
+
 
 exports.signLogin = (req,res)=>{
     res.render("signLogin");
@@ -5,6 +8,20 @@ exports.signLogin = (req,res)=>{
 
 exports.signLoginSave = (req,res)=>{
     const{name,email,password} = req.body;
-    console.log(name,email,password);
-    res.send("okay");
+    
+
+    db.query("select email from LoginData where email=?",[email],(err,responce)=>{
+     if(!err){
+        if(responce<0){
+             res.render("signLogin",{msg:"User Already Exist!"});
+        }
+        else{
+          db.query("insert into LoginData(name,email,password) values (?,?,?)",[name,email,password],(err,result)=>{
+            if(!err){
+                res.send("success");
+            }  
+        })
+        }
+    }
+    })
 }
---
+++ server/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Mysql@1010",
+  database: "crud-operations"
+});
+
+ module.exports = db;
