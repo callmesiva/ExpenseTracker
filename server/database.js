9f6f52d8a585913f0fdae255b63f1ff8c4cd06c1
callmesiva <sgnathan10@gmail.com>
2022-12-30 16:19:45 +0530

    db spell change

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DATABASE_HOSt,
+  host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME
